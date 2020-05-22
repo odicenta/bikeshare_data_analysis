03cb150a898e776c30235ccc097399e5f734a559
odicenta <odicenta@gmail.com>
2020-05-23 00:02:14 +0200

    Main logic complete, need to address issues
    The main logic is complete, need to be completed.
    Issue: when dataframe is empty, error is not being handled

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,12 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+list_of_cities = ['chicago', 'new york city', 'washington' ]
+list_of_months = ['all', 'january', 'february', 'march', 'april', 'may', 'june',
+                  'july', 'august', 'september', 'october', 'november', 'december']
+list_of_days = ['all', 'monday', 'tuesday', 'wednesday', 
+                'thursday', 'friday', 'saturday', 'sunday']
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -16,28 +22,30 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    list_of_cities = ['chicago', 'ch', 'new york', 'ny', 'washington', 'dc']
-    list_of_months = ['january', 'february', 'march', 'april', 'may', 'june',
-                      'july', 'august', 'september', 'october', 'november', 'december']
-    list_of_days = ['monday', 'mo', 'tuesday', 'tu', 'wednesday', 'we',
-                    'thursday', 'th', 'friday', 'fr', 'saturday', 'sa', 'sunday', 'su']
+
 
     city, month, day = False, False, False
 
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while city == False:
-        city = str.lower(input("What city would you like to study? (Chicago - CH, New York - NY or Washington - DC: "))
+        city = str.lower(input("What city would you like to study? (Chicago, New York City or Washington: "))
         if city not in list_of_cities:
+            print('Please, check your input and try again (valid options: [Chicago, New York City, Washington]')
             city = False
 
     # get user input for month (all, january, february, ... , june)
     while month == False:
-        month = str.lower(input("What month do you want to study (use full name or month number (1-12): "))
-        if month not in list_of_months and int(month) not in list(range(1,13)):
+        month = str.lower(input("What month do you want to study (use full name): "))
+        if month not in list_of_months: 
+            print('Please, check your input and try again, make sure you use [all] or the full month name')
             month = False
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-    day = None # TODO
+    while day == False:
+        day = str.lower(input('What day do you want to study (all, or name of the day): '))
+        if day not in list_of_days:
+            print('Please, check your input and try again, make sure you input [all] or the full name of the day')
+            day = False
 
     print('-'*40)
     return city, month, day
@@ -54,7 +62,35 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    print(" ".join([city, month, day]))
+    # load data file into a dataframe
+    df = pd.read_csv(CITY_DATA[city])
 
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.day_name()
+    print(df)
+    # filter by month if applicable
+    if month != 'all':
+        # use the index of the months list to get the corresponding int
+        month = list_of_months.index(month) 
+        print(month) 
+        # filter by month to create the new dataframe
+        df = df[df['month'] == month]
+    print(df)
+    # TODO: notice that there are some months that do not have data, as starting in july for chicago
+    # need to add some logic to cover this issue or will provide a Key error as the dataframe is empty
+    # Maybe after the filtering, a recount of how much data is available for this particular
+    # month and day can provide insight and help with the logic.
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        df = df[df['day_of_week'] == day.title()]
+    print(df)
 
     return df
 
@@ -66,13 +102,13 @@ def time_stats(df):
     start_time = time.time()
 
     # display the most common month
-
+    print('The most common month of travel is: {}'.format(df['month'].mode()[0]))
 
     # display the most common day of week
-
+    print('The most common day of travel of the week is: {}'.format(df['day_of_week'].mode()[0]))
 
     # display the most common start hour
-
+    print('The most common start hour of travel is: {}'.format(df['Start Time'].dt.hour.mode()[0]))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -85,10 +121,10 @@ def station_stats(df):
     start_time = time.time()
 
     # display most commonly used start station
-
+    print('\nThe most common start station is: {}'.format(df['Start Station'].mode()[0]))
 
     # display most commonly used end station
-
+    print('\nThe most common end station is: {}'.format(df['End Station'].mode()[0]))
 
     # display most frequent combination of start station and end station trip
 
